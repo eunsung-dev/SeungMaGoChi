0ed60c68e00f8c252bc3c74b4e04b13cb45627a2
Eunsung Choi <ens454@naver.com>
2023-03-26 15:39:02 +0900

    Chore: 프로젝트 세팅

---
+++ SeungMaGoChi Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SeungMaGoChi Watch App
+//
+//  Created by 최은성 on 2023/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SeungMaGoChi Watch App/SeungMaGoChiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeungMaGoChiApp.swift
+//  SeungMaGoChi Watch App
+//
+//  Created by 최은성 on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SeungMaGoChi_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
