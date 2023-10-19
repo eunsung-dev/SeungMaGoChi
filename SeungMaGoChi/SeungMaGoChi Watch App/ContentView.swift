63bed56f515043d2cc1e133df3090e24cc6d4071
Eunsung Choi <ens454@naver.com>
2023-10-19 22:39:12 +0900

    Design: 버튼 UI 변경

--- SeungMaGoChi/SeungMaGoChi Watch App/ContentView.swift
+++ SeungMaGoChi/SeungMaGoChi Watch App/ContentView.swift
@@ -10,6 +10,7 @@ import CoreMotion
 
 struct ContentView: View {
     @ObservedObject var pedometerViewModel = PedometerViewModel()
+    @State private var steps = 0
     var body: some View {
         VStack {
             HStack {
@@ -23,12 +24,15 @@ struct ContentView: View {
                 .frame(width: 35, height: 35)
             }
             Button(action: {
+                withAnimation(.easeInOut(duration: 1.0)) { // 원하는 애니메이션 지속 시간 설정
+                    steps += 1
+                }
             }) {
-                Circle()
-                    .fill(.blue)
+                Image(systemName: steps > 10 ? "star.fill" : "star")
+                    .resizable()
+                    .scaledToFit()
             }
-            .buttonStyle(PlainButtonStyle())
-
+            .clipShape(Circle())
         }
         .padding()
         .onAppear {
