ace7e8468b6d85fc004c802ef771dcee73d3fed6
Eunsung Choi <ens454@naver.com>
2023-10-19 23:07:47 +0900

    Feat: 먹이 버튼 쿨타임 기능 추가

--- SeungMaGoChi/SeungMaGoChi Watch App/ContentView.swift
+++ SeungMaGoChi/SeungMaGoChi Watch App/ContentView.swift
@@ -11,17 +11,25 @@ import CoreMotion
 struct ContentView: View {
     @ObservedObject var pedometerViewModel = PedometerViewModel()
     @State private var steps = 0
+    
+    @AppStorage("lastClicked") private var lastClickedInterval: TimeInterval = .zero
+    
+    @State private var isButtonDisabled = false
+    
     var body: some View {
         VStack {
             HStack {
                 Spacer()
                 Button(action: {
+                    self.setLastClickedDate(date: Date()) // 현재 시간 저장
+                    checkButtonStatus()
                 }) {
                     Circle()
                         .fill(.blue)
                 }
                 .buttonStyle(PlainButtonStyle())
                 .frame(width: 35, height: 35)
+                .disabled(isButtonDisabled)
             }
             Button(action: {
                 withAnimation(.easeInOut(duration: 1.0)) { // 원하는 애니메이션 지속 시간 설정
@@ -35,12 +43,39 @@ struct ContentView: View {
             .clipShape(Circle())
         }
         .padding()
-        .onAppear {
-            DispatchQueue.main.async {
-                pedometerViewModel.initializePedometer()
-            }
-        }
-    }
+        .onAppear(perform:{
+             DispatchQueue.main.async{
+                  pedometerViewModel.initializePedometer()
+             }
+         })
+        
+       
+     }
+
+     func setLastClickedDate(date: Date) {
+         lastClickedInterval = date.timeIntervalSince1970
+     }
+
+     func getLastClickedDate() -> Date {
+         return Date(timeIntervalSince1970:lastClickedInterval)
+     }
+
+     func checkButtonStatus() {
+         let timeSinceLastClick = Date().timeIntervalSince(getLastClickedDate())
+
+         if timeSinceLastClick < 4*60*60 { // 4시간 경과 미만일 경우
+             isButtonDisabled = true
+
+             let remainingTime = 4*60*60 - timeSinceLastClick
+
+             DispatchQueue.main.asyncAfter(deadline:.now()+remainingTime) {
+                 self.isButtonDisabled=false
+             }
+
+         } else{
+              isButtonDisabled=false
+          }
+      }
 }
 
 struct ContentView_Previews: PreviewProvider {
